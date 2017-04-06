22285b080fa9a110b0642c536b7644bbf842fd18
Cameron Bullock <bullockcameron@gmail.com>
2017-04-05 22:43:02 -0600

    final changes

--- presentation/index.js
+++ presentation/index.js
@@ -13,7 +13,11 @@ import {
  Slide,
  Image,
  Text,
- Layout
+ Layout,
+ Appear,
+ Typeface,
+ Fit,
+ Fill
 } from "spectacle";
 
 // Import image preloader util
@@ -29,12 +33,32 @@ require("spectacle/lib/themes/default/index.css");
 
 const images = {
  city: require("../assets/city.jpg"),
- logo: require("../assets/formidable-logo.svg"),
+ logo: require("../assets/wb_favicon.png"),
  markdown: require("../assets/markdown.png"),
+
+ // Slide Images
+ homeImage: require("../assets/images/Home.png"),
+ dashboardImage: require("../assets/images/Dashboard_article.png"),
+ exploreImage: require("../assets/images/Explore_no_media.png"),
+ addMedia: require("../assets/images/addMedia.png"),
+ readerMode: require("../assets/images/ReaderMode.png"),
+ accountImage: require("../assets/images/accountPage.png"),
+ signupImage: require("../assets/images/signupImage.png"),
+ road: require("../assets/images/road.jpg"),
+
+ // Team Images
+ addison: require("../assets/TeamImages/AddisonProfile.jpg"),
+ cameron: require("../assets/TeamImages/CameronProfile.jpg"),
+ tyler: require("../assets/TeamImages/TylerProfile.jpg"),
+ group: require("../assets/TeamImages/wisebiasGroup.jpg"),
+
+ // Gifs
  signup: require("../assets/AppGifs/SignupClose.gif"),
- landing: require("../assets/AppGifs/LandingPage.png"),
  dashboard: require("../assets/AppGifs/Dashboard.gif"),
  account: require("../assets/AppGifs/account.gif"),
+
+//Logos
+ techLogos: require("../assets/logos/techLogos.png"),
  express: require("../assets/logos/express.png"),
  git: require("../assets/logos/Git-Logo.png"),
  github: require("../assets/logos/github.png"),
@@ -46,10 +70,6 @@ const images = {
  react: require("../assets/logos/React.png"),
  redux: require("../assets/logos/reduxlogo.png"),
  vanillajs: require("../assets/logos/VanillaJS.png"),
- addison: require("../assets/TeamImages/AddisonProfile.jpg"),
- cameron: require("../assets/TeamImages/CameronProfile.jpg"),
- tyler: require("../assets/TeamImages/TylerProfile.jpg"),
- group: require("../assets/TeamImages/wisebiasGroup.jpg")
 };
 
 preloader(images);
@@ -57,7 +77,7 @@ preloader(images);
 const theme = createTheme({
  primary: "White",
  secondary: "#FF2D55",
- tertiary: "#FF2D55",
+ tertiary: "#2D79FF",
  quartenary: "#CECECE"
 }, {
  primary: "Montserrat",
@@ -67,62 +87,77 @@ const theme = createTheme({
 export default class Presentation extends React.Component {
  render() {
    return (
-     <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-       <Slide transition={["fade"]} bgColor="tertiary">
-         <Layout>
-           <Heading size={6} textColor="primary">Cameron Bullock</Heading>
-           <Heading style={{paddingLeft:"10", paddingRight:"10"}} size={6} textColor="primary">Tyler Foss</Heading>
-           <Heading size={6} textColor="primary">Addison Fuller</Heading>
-        </Layout>
-        <Layout>
-          <img style={{margin: "auto", width: "400", height:"400", display:"inline", paddingLeft: "50", paddingRight: "50" }} src={images.cameron} />
-          <img style={{margin: "auto", width: "400", height:"400", display:"inline", paddingLeft: "50", paddingRight: "50" }} src={images.tyler} />
-          <img style={{margin: "auto", width: "400", height:"400", display:"inline", paddingLeft: "50", paddingRight: "50" }} src={images.addison} />
-        </Layout>
+     <Deck progress="bar" transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+       <Slide transition={["fade"]} bgColor="secondary">
+          <Heading size={2} fit caps lineHeight={1} textColor="primary">
+            Our Team
+          </Heading>
+          <Layout>
+            <Fill>
+               <img margin="0px auto 40px" height="293px" src={images.cameron} />
+               <Heading size={6} textColor="primary">Cameron Bullock</Heading>
+               <Text textSize="25" textColor="primary">Github: CameronBullock</Text>
+            </Fill>
+            <Fill>
+               <img margin="0px auto 40px" height="293px" src={images.tyler} />
+               <Heading size={6} textColor="primary">Tyler<br/>Foss</Heading>
+               <Text textSize="25" textColor="primary">Github: TylerFoss</Text>
+            </Fill>
+            <Fill>
+               <img margin="0px auto 40px" height="293px" src={images.addison} />
+               <Heading size={6} textColor="primary">Addison Fuller</Heading>
+               <Text textSize="25" textColor="primary">Github: addisoncharlesfuller</Text>
+            </Fill>
+         </Layout>
        </Slide>
        <Slide transition={["zoom"]} bgColor="primary">
          <Heading size={1} fit caps lineHeight={1} textColor="black">
            Wise Bias
          </Heading>
-         <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-           Changing Your Bias One Click At A Time!
+         <Text margin="10px 0 0" textColor="secondary" size={1} fit bold>
+          A Place To Expand Your Point of View
          </Text>
        </Slide>
-       <Slide transition={["fade"]} bgColor="secondary" textColor="tertiary">
-
-       <Heading size={3} textColor="Black">Home Page</Heading>
-       <img style={{ width: "900", marginTop: "50", }} src={images.landing} />
-
+       <Slide transition={["slide"]} bgColor="tertiary">
+          <Heading size={3} textColor="white">Why Wisebias?</Heading>
        </Slide>
-       <Slide transition={["fade"]} bgColor="secondary" textColor="tertiary">
 
-         <Heading size={3} textColor="Black">Sign Up</Heading>
-         <img style={{ width: "900", marginTop: "50", }} src={images.signup} />
+       <Slide transition={["slide"]} bgImage={images.signupImage}></Slide>
+       <Slide transition={["slide"]} bgImage={images.signup}></Slide>
 
+       <Slide transition={["slide"]} bgColor="secondary">
+          <Heading size={3} textColor="white">Explore</Heading>
        </Slide>
-       <Slide transition={["fade"]} bgColor="secondary" textColor="tertiary">
-         <Heading size={3} textColor="Black">Dashboard</Heading>
-         <img style={{ width: "100%", marginTop: "50", }} src={images.dashboard} />
-       </Slide>
-       <Slide transition={["fade"]} bgColor="secondary" textColor="tertiary">
-         <Heading size={3} textColor="Black">Account</Heading>
-         <img style={{ width: "100%", marginTop: "50", }} src={images.account} />
-       </Slide>
-       <Slide transition={["fade"]} bgColor="White" textColor="tertiary">
-         <Heading size={3} textColor="Black">Technologies</Heading>
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.express} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.git} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.github} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.materialize} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.mongodb} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.mongoose} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.node} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.reactrouter} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.react} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.redux} />
-         <img style={{ width: "200", display:"inline", marginTop: "20", marginBottom: "20", paddingLeft: "20", paddingRight: "20" }} src={images.vanillajs} />
 
+       <Slide transition={["slide"]} bgImage={images.dashboardImage}></Slide>
+       <Slide transition={["slide"]} bgImage={images.readerMode}></Slide>
+       <Slide transition={["slide"]} bgImage={images.dashboard}></Slide>
+
+       <Slide transition={["slide"]} bgColor="secondary">
+          <Heading size={3} textColor="white">Account</Heading>
        </Slide>
+
+       <Slide transition={["slide"]} bgImage={images.accountImage}></Slide>
+
+       <Slide transition={["slide"]} bgImage={images.techLogos}>
+        </Slide>
+
+       <Slide transition={["slide"]} bgImage={images.road} bgDarken={0.75}>
+            <Heading size={1} caps fit textColor="primary">
+              Our Roadmap
+            </Heading>
+            <Typeface weight={200}>
+               <Text id="sub-text" caps textColor="#d0d0d0" >Looking Ahead</Text>
+            </Typeface>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="black">
+           Wise Bias
+          </Heading>
+          <Text margin="10px 0 0" textColor="secondary" size={1} fit bold>
+           www.wisebias.com
+          </Text>
+        </Slide>
      </Deck>
    );
  }
